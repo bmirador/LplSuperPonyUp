53de9d41d31334ac803e6749e5744a515decf411
Everardo <everardo.esparza@cubix.com.mx>
2025-06-14 22:51:26 -0600

    Added icon to the app and splash screen Added dividers to items on CommentsListScreen.kt

---
+++ app/src/main/java/com/redprisma/lplsuperponyup/ui/screens/CommentItem.kt
@@ -0,0 +1,116 @@
+package com.redprisma.lplsuperponyup.ui.screens
+
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.redprisma.lplsuperponyup.R
+
+@Composable
+fun CommentItem(
+    modifier: Modifier = Modifier,
+    id: Int,
+    name: String,
+    email: String,
+    body: String
+) {
+    var imageUri by rememberSaveable { mutableStateOf<Uri?>(null) }
+
+    val imagePickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            imageUri = uri
+        }
+    }
+
+    Row(
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier.Companion
+                .size(48.dp)
+                .align(Alignment.Companion.Top)
+                .clip(CircleShape)
+                .clickable { imagePickerLauncher.launch("image/*") }
+        ) {
+            if (imageUri != null) {
+                AsyncImage(
+                    model = imageUri,
+                    contentDescription = stringResource(R.string.user_icon),
+                    contentScale = ContentScale.Companion.Crop,
+                    modifier = Modifier.Companion
+                        .fillMaxSize()
+                        .clip(CircleShape)
+                )
+            } else {
+                Icons.Default.AccountCircle.tintColor
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = stringResource(R.string.user_icon),
+                    modifier = Modifier.Companion
+                        .fillMaxSize()
+                        .clip(CircleShape),
+                    tint = Color.LightGray
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.Companion.width(12.dp))
+
+        Column(modifier = Modifier.Companion.weight(1f)) {
+            Row(
+                modifier = Modifier.Companion.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    modifier = Modifier.Companion.weight(1f),
+                    text = name,
+                    style = MaterialTheme.typography.bodyMedium,
+                    overflow = TextOverflow.Companion.Ellipsis
+                )
+                Text(text = email, style = MaterialTheme.typography.bodySmall)
+            }
+
+            Text(
+                text = stringResource(R.string.id, id),
+                style = MaterialTheme.typography.labelMedium
+            )
+            Spacer(modifier = Modifier.Companion.height(4.dp))
+            Text(text = body, style = MaterialTheme.typography.bodySmall)
+        }
+    }
+}
--- app/src/main/java/com/redprisma/lplsuperponyup/ui/screens/CommentsListScreen.kt
+++ app/src/main/java/com/redprisma/lplsuperponyup/ui/screens/CommentsListScreen.kt
@@ -1,45 +1,24 @@
 package com.redprisma.lplsuperponyup.ui.screens
 
-import android.net.Uri
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Arrangement.Center
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.redprisma.lplsuperponyup.HomeState
 import com.redprisma.lplsuperponyup.R
 import com.redprisma.lplsuperponyup.data.models.Comment
@@ -83,23 +62,31 @@ fun CommentListScreen(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 if (homeState.comments.isEmpty()) {
-                    Text("No comments were found")
+                    Text(stringResource(R.string.no_comments_were_found))
                 } else {
                     if (homeState.fromCache) {
-                        Text("Showing results from cache, there was a problem retrieving the latest comments")
+                        Text(stringResource(R.string.showing_results_from_cache_there_was_a_problem_retrieving_the_latest_comments))
                     }
                     LazyColumn {
-                        items(
-                            items = homeState.comments,
-                            key = { it?.id ?: Uuid.random() }) { comment ->
-                            comment?.run {
-                                // Display individual comment item
-                                CommentItem(
-                                    id = id,
-                                    name = name,
-                                    email = email,
-                                    body = body
-                                )
+                        itemsIndexed(
+                            homeState.comments,
+                            // If a comment does not have an id it probably should not be shown
+                            // this is something I would need to ask if not detailed on the
+                            // story, for now, I decided to give it a random ID if null
+                            key = { _, comment -> comment?.id ?: Uuid.random() }) { i, comment ->
+                            Column(modifier = Modifier.padding(start = 16.dp)) {
+                                comment?.run {
+                                    CommentItem(
+                                        id = id,
+                                        name = name,
+                                        email = email,
+                                        body = body
+                                    )
+                                }
+
+                                if (i < homeState.comments.lastIndex) {
+                                    HorizontalDivider(modifier, 1.dp, Color.LightGray)
+                                }
                             }
                         }
                     }
@@ -109,83 +96,6 @@ fun CommentListScreen(
         is HomeState.Error ->
             // Display error screen with retry option
             ErrorScreen(homeState.appError, loadComments)
-    }
-}
-
-
-@Composable
-fun CommentItem(
-    modifier: Modifier = Modifier,
-    id: Int,
-    name: String,
-    email: String,
-    body: String
-) {
-    var imageUri by rememberSaveable { mutableStateOf<Uri?>(null) }
-
-    val imagePickerLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.GetContent()
-    ) { uri: Uri? ->
-        if (uri != null) {
-            imageUri = uri
-        }
-    }
-
-    Row(
-        modifier = modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-    ) {
-        Box(
-            modifier = Modifier
-                .size(48.dp)
-                .align(Alignment.Top)
-                .clip(CircleShape)
-                .clickable { imagePickerLauncher.launch("image/*") }
-        ) {
-            if (imageUri != null) {
-                AsyncImage(
-                    model = imageUri,
-                    contentDescription = stringResource(R.string.user_icon),
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(CircleShape)
-                )
-            } else {
-                Icon(
-                    imageVector = Icons.Default.AccountCircle,
-                    contentDescription = stringResource(R.string.user_icon),
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(CircleShape)
-                )
-            }
-        }
-
-        Spacer(modifier = Modifier.width(12.dp))
-
-        Column(modifier = Modifier.weight(1f)) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = name,
-                    style = MaterialTheme.typography.bodyMedium,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(text = email, style = MaterialTheme.typography.bodySmall)
-            }
-
-            Text(
-                text = stringResource(R.string.id, id),
-                style = MaterialTheme.typography.labelSmall
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = body, style = MaterialTheme.typography.bodySmall)
-        }
     }
 }
 
